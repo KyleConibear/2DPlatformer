2799dcbeadc5aac1f3a92df23676b181b5d2dff9
Kyle Conibear <kyle@gamepill.com>
2020-10-06 20:57:44 -0400

    Tile maps and player jump

--- 2DPlatformer/Assets/Player.cs
+++ 2DPlatformer/Assets/Player.cs
@@ -2,11 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rigidbody2D = null;
-    [SerializeField] private float force = 5;
-    [SerializeField] private float xMove = 0;
+    private Rigidbody2D rigidbody2D = null;
+
+    [Range(100, 250)]
+    [SerializeField] private float force = 150;
+
+    [Range(250, 1000)]
+    [SerializeField] private float jumpHeight = 500;
+
+    private void Jump() // Passing variable into method
+    {
+        Debug.Log("Jump");
+        rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, (jumpHeight * Time.deltaTime));
+    }
+
+    private void Move(float xInput)
+    {   
+        rigidbody2D.velocity = new Vector2((xInput * force * Time.deltaTime), rigidbody2D.velocity.y);
+    }
+
+    // Opening Tag
+    #region MonoBehaviour Methods
 
     private void Awake()
     {
@@ -15,11 +34,17 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        xMove = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space)) // GetKeyDown happens once. -> KeyCode.Space is the spacebar
+        {
+            Jump();
+        }
     }
 
     private void FixedUpdate()
     {
-        rigidbody2D.velocity = new Vector2(xMove * force * Time.deltaTime, 0);
+        this.Move(Input.GetAxis("Horizontal"));
     }
+
+    // Closing Tag
+    #endregion
 }
